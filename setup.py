6163bbf705d23b893696baf7a7a3576b272cf9fa
Mohammad Abbas <mohammad.abbas@digital.trade.gov.uk>
2020-03-23 14:09:45 +0000

    Use setuptools find_packages to get source packages

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
-#!/usr/bin/env python
-
-from distutils.core import setup
+from setuptools import find_packages, setup
 
 setup(
     name='data-engineering-common',
     version='1.0',
-    package_dir={'': 'src'},
+    packages=find_packages(exclude='tests'),
     install_requires=['python-dotenv>=0.12.0'],
 )
