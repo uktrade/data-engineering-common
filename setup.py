2122048d9aa3777a9dab5cf412d3652b6cf1706f
Mohammad Abbas <mohammad.abbas@digital.trade.gov.uk>
2020-03-23 14:24:43 +0000

    Remove top level src directory

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import find_packages, setup
 setup(
     name='data-engineering-common',
     version='1.0',
-    packages=find_packages(exclude='tests'),
+    packages=find_packages(exclude='tests.*', 'tests'),
     install_requires=['python-dotenv>=0.12.0'],
 )
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,2 +1,2 @@
 # flake8: noqa
-from src.common.tests.conftest import *
+from common.tests.conftest import *
