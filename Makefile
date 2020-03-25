381633b06fe848885bc675ea84522be5007002bd
Mohammad Abbas <mohammad.abbas@digital.trade.gov.uk>
2020-03-25 18:10:08 +0000

    Add circle ci config

---
+++ Makefile
@@ -0,0 +1,19 @@
+TEST ?=.
+COV ?= --cov
+BLACK_CONFIG ?= --exclude=venv --skip-string-normalization --line-length 100
+CHECK ?= --check
+
+.PHONY: run_tests
+run_tests:
+	TESTING=1 pytest -p no:sugar ${TEST} ${COV}
+
+check: flake8 black
+
+format: CHECK=
+format: black
+
+black:
+	black ${BLACK_CONFIG} ${CHECK} .
+
+flake8:
+	flake8 .
--- setup.py
+++ setup.py
@@ -5,7 +5,10 @@ setup(
     version='1.0',
     packages=find_packages(exclude=['tests.*', 'tests']),
     install_requires=[
+        'black==19.10b0',
         'certifi>=2019.11.28',
+        'flake8-import-order==0.18.1',
+        'flake8==3.7.9',
         'flask==1.1.1',
         'flask-sqlalchemy==2.4.1',
         'freezegun>=0.3.15',
