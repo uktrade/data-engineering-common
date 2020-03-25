13c2d02288d0ec603b876e5012fdcecf781f7d43
Mohammad Abbas <mohammad.abbas@digital.trade.gov.uk>
2020-03-25 18:23:28 +0000

    Add broken test

--- tests/views/test_healthcheck.py
+++ tests/views/test_healthcheck.py
@@ -1,5 +1,5 @@
 def test_healthcheck_view(app_with_db):
     client = app_with_db.test_client()
     response = client.get('/healthcheck/')
-    assert response.status_code == 200
+    assert response.status_code == 400
     assert response.json == {'status': 'OK'}
