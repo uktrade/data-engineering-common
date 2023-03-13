6affdc95d656ff8985e2e2e9f26be98f3f21340c
nboyse <natasha.boyse@digital.trade.gov.uk>
2023-03-13 11:23:39 +0000

    bump: sqlalchemy and werkzeug

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
         'requests>=2.22.0',
         'requests-oauthlib>=1.1.0,<1.2.0',
         'sqlalchemy-utils>=0.36.1',
-        'sqlalchemy<2.0.0',
-        'werkzeug>=0.16.1',
+        'sqlalchemy>=2.0.0',
+        'werkzeug>=2.2.3',
     ],
 )
